38527158f1d2aa57f726587ea05ec9569d45ab0d
Qi Zhang <qizhang@Qis-MacBook-Pro.local>
2017-05-04 00:26:36 -0700

    init

---
+++ app/app.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
+
+// load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
