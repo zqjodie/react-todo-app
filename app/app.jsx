9efba20c640e40ad34f2a0a9db8ff60f1c6a89e5
Qi Zhang <qizhang@Qis-MacBook-Pro.local>
2017-05-04 21:25:52 -0700

    add todo app

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Timer = require('Timer');
-var Countdown = require('Countdown');
+
+var TodoApp = require('TodoApp');
 
 // load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
@@ -12,11 +11,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      <Route path="countdown" component={Countdown}/>
-      <IndexRoute component={Timer}/>
-    </Route>
-  </Router>,
+  <TodoApp/>,
   document.getElementById('app')
 );
