d478834284c5ea3721863e28d29c231eed3c5cd2
Qi Zhang <qizhang@Qis-MacBook-Pro.local>
2017-05-10 00:17:31 -0700

    add todo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,20 +1,45 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var TodoAddItem = require('TodoAdd');
+var TodoSearch= require('TodoSearch');
+var uuid = require('uuid');
+
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [
-        {id: 1, text: 'Walk the dog'},
-        {id: 2, text: 'Clean'}
+        {id: uuid(), text: 'Walk the dog'},
+        {id: uuid(), text: 'Clean'}
       ]
     }
+  },
+  handleAddTodo: function (text) {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    })
+  },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    })
   },
   render: function () {
     var {todos} = this.state;
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
+        <TodoAddItem handleAddTodo={this.handleAddTodo}/>
       </div>
     )
   }
